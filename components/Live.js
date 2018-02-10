2eb7cbe4621e417a22d245ddb24b11197e5a91b5
ubbn <ubbn@live.com>
2018-02-10 20:50:20 +0100

    Geolocation and animation feature is added

--- components/Live.js
+++ components/Live.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react'
-import { View, Text, ActivityIndicator, TouchableOpacity, StyleSheet } from 'react-native'
+import { View, Text, ActivityIndicator, TouchableOpacity, StyleSheet, Animated } from 'react-native'
 import { Foundation } from '@expo/vector-icons'
 import { Location, Permissions } from 'expo'
 
-import { purple, white } from '../utils/colors'
 import { calculateDirection } from '../utils/helpers'
+import { purple, white } from '../utils/colors'
 
 export default class Live extends Component {
   state = {
-    coords: null,
+    coords: { altitude: 1, speed: 1} ,
     status: null,
-    direction: ''
+    direction: '',
+    bounceValue: new Animated.Value(1),
   }
-
   componentDidMount () {
     Permissions.getAsync(Permissions.LOCATION)
       .then(({ status }) => {
@@ -28,11 +28,17 @@ export default class Live extends Component {
         this.setState(() => ({ status: 'undetermined' }))
       })
   }
-
   askPermission = () => {
-  
-  }
+    Permissions.askAsync(Permissions.LOCATION)
+      .then(({ status }) => {
+        if (status === 'granted') {
+          return this.setLocation()
+        }
 
+        this.setState(() => ({ status }))
+      })
+      .catch((error) => console.warn('error asking Location permission: ', error))
+  }
   setLocation = () => {
     Location.watchPositionAsync({
       enableHighAccuracy: true,
@@ -40,7 +46,14 @@ export default class Live extends Component {
       distanceInterval: 1,
     }, ({ coords }) => {
       const newDirection = calculateDirection(coords.heading)
-      const { direction } = this.state
+      const { direction, bounceValue } = this.state
+
+      if (newDirection !== direction) {
+        Animated.sequence([
+          Animated.timing(bounceValue, { duration: 200, toValue: 1.04}),
+          Animated.spring(bounceValue, { toValue: 1, friction: 4})
+        ]).start()
+      }
 
       this.setState(() => ({
         coords,
@@ -48,10 +61,9 @@ export default class Live extends Component {
         direction: newDirection,
       }))
     })
-  }  
-
+  }
   render() {
-    const { status, coords, direction } = this.state
+    const { status, coords, direction, bounceValue } = this.state
 
     if (status === null) {
       return <ActivityIndicator style={{marginTop: 30}}/>
@@ -88,9 +100,10 @@ export default class Live extends Component {
       <View style={styles.container}>
         <View style={styles.directionContainer}>
           <Text style={styles.header}>You're heading</Text>
-          <Text style={styles.direction}>
-            North
-          </Text>
+          <Animated.Text
+            style={[styles.direction, {transform: [{scale: bounceValue}]}]}>
+              {direction}
+          </Animated.Text>
         </View>
         <View style={styles.metricContainer}>
           <View style={styles.metric}>
@@ -98,7 +111,7 @@ export default class Live extends Component {
               Altitude
             </Text>
             <Text style={[styles.subHeader, {color: white}]}>
-              {200} feet
+              {Math.round(coords.altitude * 3.2808)} Feet
             </Text>
           </View>
           <View style={styles.metric}>
@@ -106,7 +119,7 @@ export default class Live extends Component {
               Speed
             </Text>
             <Text style={[styles.subHeader, {color: white}]}>
-              {300} MPH
+              {(coords.speed * 2.2369).toFixed(1)} MPH
             </Text>
           </View>
         </View>
@@ -134,7 +147,7 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     margin: 20,
   },
-  buttonText: {
+  buttonText :{
     color: white,
     fontSize: 20,
   },
@@ -170,5 +183,5 @@ const styles = StyleSheet.create({
     fontSize: 25,
     textAlign: 'center',
     marginTop: 5,
-  }
+  },
 })
